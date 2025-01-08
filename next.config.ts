f33317a986d737c0d10adb7ac8a5e4557ea22f95
KissuChaudhary <97441130+KissuChaudhary@users.noreply.github.com>
2025-01-08 22:16:55 +0530

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,7 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  swcMinify: true,
 };
 
 export default withNextIntl(config);
